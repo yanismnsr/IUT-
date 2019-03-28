19059e5d6227cad0b6eb51284d1629c50ccd969a
yanismnsr <44984759+yanismnsr@users.noreply.github.com>
2019-03-28 12:10:13 +0100

    Update roles.sql

--- bdd/roles.sql
+++ bdd/roles.sql
@@ -1,32 +1,37 @@
-create or replace function post(destinataire varchar, message text) returns boolean as
+create or replace function post (destinataire varchar, message text) returns boolean as
   $$
     declare
-      dest name;
+      dest varchar;
     begin
-      select rolname into dest from pg_roles where rolcanlogin and rolname::varchar = destinataire;
+      select rolname into dest from pg_roles where rolcanlogin and rolname = destinataire;
       if not found then
-        raise 'user % does not exist', destinataire;
+        raise notice 'destinataire "%" introuvable', destinataire;
         return false;
+      else
+        insert into m_envoyes(message, expediteur, destinataire) values (message, session_user, dest);
+        insert into m_recus(message, expediteur, destinataire) values (message, session_user, dest);
+        return true;
       end if;
-      insert into m_envoyes (message, expediteir, destinataire) values (message, current_user, destinataire);
-      insert into m_recus (message, expediteur, destinataire) values (message, current_user, destinataire);
-      return true;
     end;
   $$ language plpgsql SECURITY DEFINER;
 
 
-create or replace function get(out num int, out expe name, out quand timestamp, out mess text) returns setof record as
+
+create or replace function get (out num int, out expe name, out quand timestamp, out mess text) returns setof record as
   $$
     declare
-      curseur cursor for select numero, expediteur, date_envoie, message from m_recus where destinataire = current_user;
+      mail cursor for
+      select numero, expediteur, date_envoie, '<-' || message from m_recus where destinataire = session_user
+      union
+      select numero, destinataire, date_envoie, '->' || message from m_envoyes where expediteur = session_user order by date_envoie;
     begin
-      open curseur;
+      open mail;
       loop
-        select numero, expediteur, date_envoie, message into num, expe, quand, mess from m_recus where destinataire = current_user;
+        fetch mail into num, expe, quand, mess;
         exit when not found;
         return next;
       end loop;
-      close curseur;
       return;
+      close mail;
     end;
-  $$ language plpgsql security definer;
+  $$ language plpgsql SECURITY DEFINER;
